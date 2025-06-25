8864dc423b7aebf32f144caf38ba2fa873f435fa
Adanta Griffin <griffinadanta90@gmail.com>
2025-06-25 11:25:08 -0400

    redirect edit

--- src/components/Spotify/Spotify.js
+++ src/components/Spotify/Spotify.js
@@ -1,6 +1,6 @@
 let accessToken = "";
 let clientId = "d198b429a7924f6c8570fc173c191319";
-let redirectUrl = "https://jamonspot.surge.sh";
+let redirectUrl = "https://spotsjam.netlify.app/";
 
 
 const Spotify = {
